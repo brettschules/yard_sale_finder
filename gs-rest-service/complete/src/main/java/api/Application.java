ee0b93ce3b68f058f83a00210382019b2166e3e2
Brett Schules <brettschules@gmail.com>
2019-03-22 14:35:11 -0400

    Added logging - need to fix later

--- gs-rest-service/complete/src/main/java/api/Application.java
+++ gs-rest-service/complete/src/main/java/api/Application.java
@@ -1,12 +1,17 @@
 package api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
+	private final static Logger Logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+    	Logger.info("Starting Up");
         SpringApplication.run(Application.class, args);
     }
 }
--- gs-rest-service/complete/src/main/java/seed/UserSeed/LoadDataBase.java
+++ gs-rest-service/complete/src/main/java/seed/UserSeed/LoadDataBase.java
@@ -7,18 +7,23 @@ import java.util.Date;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import api.model.User;
-import api.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@Slf4j
 class LoadDatabase {
+	private final static Logger Logger = LoggerFactory.getLogger(LoadDatabase.class);
 
 	@Bean
-	CommandLineRunner initDatabase(UserService userService) {
+	CommandLineRunner initDatabase() {
+		
 		return args -> {
-			User user = new User("Tom", "Tommy", "Tom95", "Tommer", new Date(), new Date());
-			userService.save(user);
+			Logger.info("Hiiii");
+//			log.info("Logger enabled: Entering main \n\n");
+//			log.info("hi")
+//			log.info(new User("Tom", "Tommy", "Tom95", "Tommer", new Date(), new Date()));
 		};
 	}
 }
